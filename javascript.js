130a60a486bb6218c1cb19598791653a6099f6eb
PinRathod <pinrth2015@gmail.com>
2023-10-14 15:58:43 +0530

    File uploaded successfully

--- javascript.js
+++ javascript.js
@@ -17,4 +17,25 @@ function fun()
     document.getElementById("p1").style.display="block";
     document.getElementById("p2").style.color="red";
   }
+   else if(val=="")
+  {
+    document.getElementById("error").style.display="block";
+    document.getElementById("p1").innerHTML="Fields can not be blank!";
+    document.getElementById("sname").style.border="solid 1px red";        
+    document.getElementById("success").style.display="none";
+    document.getElementById("p2").style.display="none";
+    document.getElementById("p1").style.display="block";
+    document.getElementById("p2").style.color="red";
+  }
+  else if(text=="")
+  {
+    document.getElementById("error1").style.display="block";
+    document.getElementById("p2").innerHTML="Fields can not be blank!";
+    document.getElementById("quote").style.border="solid 1px red";        
+    document.getElementById("success1").style.display="none";
+    document.getElementById("p2").style.display="block";
+    document.getElementById("p1").style.display="none";
+    document.getElementById("p2").style.color="red";
+  }
+
 }
